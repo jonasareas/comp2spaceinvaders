5e10938315a160a3c1c0854f8bf67fb74ed978ac
rafaoliveiralopes <rafaoliveiralopes@15f65c1d-d831-0410-8f75-a758940a9575>
2007-06-01 23:09:44 +0000

    Initial import.

---
+++ teste_repositorio/src/teste/teste_class.java
@@ -0,0 +1,13 @@
+package teste;
+
+public class teste_class {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bla bla bla!");
+	}
+
+}
