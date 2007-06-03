1262a1a615719eb9502c3e5de16dc34ef05f067e
jonasareas <jonasareas@15f65c1d-d831-0410-8f75-a758940a9575>
2007-06-03 00:01:50 +0000

    Jonas já instalou e editou este arquivo.

--- trunk/teste_repositorio/src/teste/teste_class.java
+++ trunk/teste_repositorio/src/teste/teste_class.java
@@ -8,6 +8,7 @@ public class teste_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bla bla bla!");
+		System.out.println("Jonas já instalou e ditou este arquivo.");
 	}
 
 }
