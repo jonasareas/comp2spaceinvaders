8842306b0d12e71ad48fbbbba8cfd1f63023825d
jonasareas <jonasareas@15f65c1d-d831-0410-8f75-a758940a9575>
2007-06-04 20:35:55 +0000

    Teste com figuras

---
+++ spaceinvaders/src/br/ufrj/dcc/comp2/projeto/TesteMovimento.java
@@ -0,0 +1,36 @@
+package br.ufrj.dcc.comp2.projeto;
+
+import java.awt.BorderLayout;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class TesteMovimento implements ActionListener {
+
+	JFrame frame;
+	
+	public static void main (String[] args) {
+		TesteMovimento teste = new TesteMovimento();
+		teste.go();
+	}
+
+	public void go() {
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton button = new JButton("Change Colors");
+		button.addActionListener(this);
+		
+		MyDrawPanel drawPanel = new MyDrawPanel();
+		
+		frame.getContentPane().add(BorderLayout.SOUTH, button);
+		frame.getContentPane().add(BorderLayout.CENTER, drawPanel);		
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		frame.repaint();
+	}
+}
